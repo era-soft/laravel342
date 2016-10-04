5ab5ea707aa9be17d8df964b68024f05b73de6c3
era-soft <ibrahimeraslan@outlook.com.tr>
2016-10-04 04:14:13 +0300

    test case :)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://localhost/12';
+    protected $baseUrl = 'http://localhost';
 
     /**
      * Creates the application.
